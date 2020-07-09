38bff14284fef9b18ef170a47ba1b6ab984cf6de
mahith <mahithmurari@gmil.com>
2020-07-09 16:02:38 +0530

    updated manage.py

--- alpha/manage.py
+++ alpha/manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from os import environ 
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alpha.settings')
